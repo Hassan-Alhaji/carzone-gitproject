02a9711396e9536dca89f0cf424afa600c3d7dd8
Hassan Alhaji <al3ren0@hotmail.com>
2023-04-30 23:33:25 +0300

    created pages and configured navigation links

--- pages/urls.py
+++ pages/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about',views.about,name='About'),
+    path('cars',views.cars,name='Cars'),
+    path('services',views.services,name='Services'),
+    path('contact',views.contact,name='Contacts'),
 ]
